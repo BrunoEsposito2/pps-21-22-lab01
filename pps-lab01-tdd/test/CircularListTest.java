d4dc250f10c3998cfd77e76b91571c40cb6fba37
Bruno Esposito <bruno.esposito2@studio.unibo.it>
2022-03-01 13:16:29 +0100

    CircularListTest implemented

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,68 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import lab01.tdd.CircularList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    @BeforeEach
+    void setUp() {
+        circularList = new CircularList();
+    }
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @Test
+    void testAdd() {
+        LinkedList<Object> list = new LinkedList<Object>();
+        circularList.add(2);
+        list.add(2)
+        assertArrayEquals(list, circularList);
+    }
+
+    @Test
+    void testSize() {
+        assertEquals(0, circularList.size());
+        circularList.add(3);
+        assertEquals(1, circularList.size());
+        circularList.add(4);
+        assertEquals(2, circularList.size());
+    }
+
+    @Test
+    void testIsEmpty() {
+        assertTrue(circularList.isEmpty());
+        circularList.add(2);
+        assertFalse(circularList.isEmpty());
+    }
+
+    @Test
+    void testNext() {
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(2, circularList.next());
+        assertEquals(3, circularList.next());
+    }
+
+    @Test
+    void testPrevious() {
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(3, circularList.previous());
+    }
+
+    @Test
+    void testReset() {
+        circularList.add(2);
+        circularList.add(3);
+        circularList.next();
+        assertEquals(3, circularList.next());
+        circularList.reset();
+        assertEquals(2, circularList.next());
     }
 
 }
